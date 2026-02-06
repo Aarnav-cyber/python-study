6b9c2702156da5ed23b801e2e6b1a04577a596a8
Aarnav Slesh <Aarnav@aarnavs-macbook-pro.local>
2026-02-06 12:47:05 +0530

    changes to areatri.py

--- areatri.py
+++ areatri.py
@@ -4,5 +4,5 @@ side3 = float(input("Enter length of third side:"))
 
 print("calculating...")
 sp = (side1 + side2 + side3) / 2
-area = (sp * (sp-side1) * sp * (sp-side2) * (sp-side3))
-print("Area is.. %f", area)
+area = (sp * (sp-side1) * (sp-side2) * (sp-side3)) ** 0.5
+print("Area is.. ",area)
